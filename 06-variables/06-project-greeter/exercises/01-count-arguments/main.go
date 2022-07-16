d6a19a6a4f58aa20b9045533a11167b05b5af1f0
Chase <yeraongon@gmail.com>
2022-07-16 14:17:44 +0900

    Fix it again.

--- 06-variables/06-project-greeter/exercises/01-count-arguments/main.go
+++ 06-variables/06-project-greeter/exercises/01-count-arguments/main.go
@@ -27,7 +27,7 @@ import (
 
 func main() {
 	// UNCOMMENT & FIX THIS CODE
-	count := len(os.Args)
+	count := len(os.Args) - 1
 
 	// UNCOMMENT IT & THEN DO NOT TOUCH THIS CODE
 	fmt.Printf("There are %d names.\n", count)
