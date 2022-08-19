261c685824b9760aeca3214ad282e13eb6b746c2
Chase <yeraongon@gmail.com>
2022-08-19 16:23:05 +0900

    another version

---
+++ 11-if/exercises/05-movie-ratings/main2.go
@@ -0,0 +1,88 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import (
+  "fmt"
+  "os"
+  "strconv"
+)
+
+// ---------------------------------------------------------
+// STORY
+//
+//  Your boss wants you to create a program that will check
+//  whether a person can watch a particular movie or not.
+//
+//  Imagine that another program provides the age to your
+//  program. Depending on what you return, the other program
+//  will issue the tickets to the person automatically.
+//
+// EXERCISE: Movie Ratings
+//
+//  1. Get the age from the command-line.
+//
+//  2. Return one of the following messages if the age is:
+//     -> Above 17         : "R-Rated"
+//     -> Between 13 and 17: "PG-13"
+//     -> Below 13         : "PG-Rated"
+//
+// RESTRICTIONS:
+//  1. If age data is wrong or absent let the user know.
+//  2. Do not accept negative age.
+//
+// BONUS:
+//  1. BONUS: Use if statements only twice throughout your program.
+//  2. BONUS: Use an if statement only once.
+//
+// EXPECTED OUTPUT
+//  go run main.go 18
+//    R-Rated
+//
+//  go run main.go 17
+//    PG-13
+//
+//  go run main.go 12
+//    PG-Rated
+//
+//  go run main.go
+//    Requires age
+//
+//  go run main.go -5
+//    Wrong age: "-5"
+// ---------------------------------------------------------
+
+func main() {
+  const (
+    r = "R-Rated"
+    pg = "PG-13"
+    pgr = "PG-Rated"
+    err1 = "Requires age"
+    err2 = "Wrong age: "-5"
+  )
+  
+  var (
+    a = os.Args
+    g int
+    err error
+  )
+    
+  if len(a) != 2 {
+    fmt.Println("Requires age.\n")
+  } else if g, err = strconv.Atoi(a[1]); err != nil || g < 0 {
+    fmt.Printf("Wrong age: %q\n", a[1])
+  } else {
+    if g > 17 {
+      fmt.Println(r)
+    } else if g >= 13 {
+      fmt.Println(pg)
+    } else {
+      fmt.Println(pgp)
+    }
+}
