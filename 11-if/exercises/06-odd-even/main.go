441d064fa1fe69e4e7fbf7a2e0e52cb1e75c03d9
Chase <yeraongon@gmail.com>
2022-08-22 22:54:34 +0900

    Fix it

--- 11-if/exercises/06-odd-even/main.go
+++ 11-if/exercises/06-odd-even/main.go
@@ -1,10 +1,17 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
 
 package main
 
 import (
-  "fmt"
-  "os"
-  "strconv"
+	"fmt"
+	"os"
+	"strconv"
 )
 
 // ---------------------------------------------------------
@@ -36,12 +43,27 @@ import (
 // ---------------------------------------------------------
 
 const (
-  divider = 8
-  p = "%d is an %s number\n"
-  err1 = "Pick a number\n"
-  err2 = "%q is not a number\n"
+	divider = 8
+	pr1     = "%d is an even number and it's divisible by %d\n"
+	pr2     = "%d is an even number\n"
+	pr3     = "%d is an odd number\n"
+	err1    = "Pick a number\n"
+	err2    = "%q is not a number\n"
 )
 
 func main() {
-  a = os
+	// short statement
+	if a := os.Args; len(a) != 2 {
+		fmt.Printf(err1)
+	} else if n, err := strconv.Atoi(a[1]); err != nil {
+		fmt.Printf(err2, a[1])
+	} else {
+		if n%2 == 0 && n%8 == 0 {
+			fmt.Printf(pr1, n, divider)
+		} else if n%2 == 0 {
+			fmt.Printf(pr2, n)
+		} else {
+			fmt.Printf(pr3, n)
+		}
+	}
 }
