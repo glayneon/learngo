8f9406f930a2c165b867c4ef88df7a6a9c9d8763
Chase <yeraongon@gmail.com>
2022-08-26 16:51:44 +0900

    Fix it.

--- 12-switch/exercises/02-richter-scale-2/main.go
+++ 12-switch/exercises/02-richter-scale-2/main.go
@@ -1,70 +1,51 @@
-// Copyright © 2018 Inanc Gumus
-// Learn Go Programming Course
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-//
-// For more tutorials  : https://learngoprogramming.com
-// In-person training  : https://www.linkedin.com/in/inancgumus/
-// Follow me on twitter: https://twitter.com/inancgumus
-
 package main
 
-// ---------------------------------------------------------
-// EXERCISE: Richter Scale #2
-//
-//  Repeat the previous exercise.
-//
-//  But, this time, get the description and print the
-//  corresponding richter scale.
-//
-//  See the expected outputs.
-//
-// ---------------------------------------------------------
-// MAGNITUDE                    DESCRIPTION
-// ---------------------------------------------------------
-// Less than 2.0                micro
-// 2.0 to less than 3.0         very minor
-// 3.0 to less than 4.0         minor
-// 4.0 to less than 5.0         light
-// 5.0 to less than 6.0         moderate
-// 6.0 to less than 7.0         strong
-// 7.0 to less than 8.0         major
-// 8.0 to less than 10.0        great
-// 10.0 or more                 massive
-//
-// EXPECTED OUTPUT
-//  go run main.go
-//   Tell me the magnitude of the earthquake in human terms.
-//
-//  go run main.go alien
-//   alien's richter scale is unknown
-//
-//  go run main.go micro
-//   micro's richter scale is less than 2.0
-//
-//  go run main.go "very minor"
-//   very minor's richter scale is 2 - 2.9
-//
-//  go run main.go minor
-//   minor's richter scale is 3 - 3.9
-//
-//  go run main.go light
-//   light's richter scale is 4 - 4.9
-//
-//  go run main.go moderate
-//   moderate's richter scale is 5 - 5.9
-//
-//  go run main.go strong
-//   strong's richter scale is 6 - 6.9
-//
-//  go run main.go major
-//   major's richter scale is 7 - 7.9
-//
-//  go run main.go great
-//   great's richter scale is 8 - 9.9
-//
-//  go run main.go massive
-//   massive's richter scale is 10+
-// ---------------------------------------------------------
+import (
+  "os"
+  "fmt"
+  "strings"
+)
+
+const (
+  err1 = "Tell me the magnitude of the earthquake in human terms.\n"
+  err2 = "%s's richter scale is unknown\n"
+  pr1 = "%s's richter scale is %s\n"
+  micro = "less than 2.0\n"
+  vminor = "2 - 2.9\n"
+  minor = "3 - 3.9\n"
+  light = "4 - 4.9\n"
+  mod = "5 - 5.9\n"
+  strong = "6 - 6.9\n"
+  major = "7 - 7.9\n"
+  great = "8 - 9.9\n"
+  mass = "10+\n"
+)
 
 func main() {
+  if len(os.Args) != 2 {
+    fmt.Printf(err1)
+    return
+  }
+  
+  switch n := strings.ToLower(os.Args[1]); {
+  case "micro":
+    fmt.Printf(pr1, n, micro)
+  case "very minor":
+    fmt.Printf(pr1, n, vminor)
+  case "minor":
+    fmt.Printf(pr1, n, minor)
+  case "light":
+    fmt.Printf(pr1, n, light)
+  case "moderate":
+    fmt.Printf(pr1, n, mod)
+  case "strong":
+    fmt.Printf(pr1, n, strong)
+  case "major":
+    fmt.Printf(pr1, n, major)
+  case "great":
+    fmt.Printf(pr1, n, great)
+  case "massive":
+    fmt.Printf(pr1, n, mass)
+  default:
+    fmt.Printf(err2, n)
 }
