0855c5830ffed3d98f02cd7138085ccc82a2cf9f
Chase <yeraongon@gmail.com>
2022-08-26 18:51:57 +0900

    Fix it

--- 12-switch/exercises/04-string-manipulator/main.go
+++ 12-switch/exercises/04-string-manipulator/main.go
@@ -1,48 +1,37 @@
-// Copyright © 2018 Inanc Gumus
-// Learn Go Programming Course
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-//
-// For more tutorials  : https://learngoprogramming.com
-// In-person training  : https://www.linkedin.com/in/inancgumus/
-// Follow me on twitter: https://twitter.com/inancgumus
-
 package main
 
-// ---------------------------------------------------------
-// STORY
-//  You want to write a program that will manipulate a
-//  given string to uppercase, lowercase, and title case.
-//
-// EXERCISE: String Manipulator
-//
-//  1. Get the operation as the first argument.
-//
-//  2. Get the string to be manipulated as the 2nd argument.
-//
-// HINT
-//  You can find the manipulation functions in the strings
-//  package Go documentation (ToLower, ToUpper, Title).
-//
-// EXPECTED OUTPUT
-//
-//  go run main.go
-//    [command] [string]
-//
-//    Available commands: lower, upper and title
-//
-//  go run main.go lower 'OMG!'
-//    omg!
-//
-//  go run main.go upper 'omg!'
-//    OMG!
-//
-//  go run main.go title "mr. charles darwin"
-//    Mr. Charles Darwin
-//
-//  go run main.go genius "mr. charles darwin"
-//    Unknown command: "genius"
-// ---------------------------------------------------------
+import (
+  "fmt"
+  "os"
+  "strings"
+)
+
+const (
+  usage = `
+string-man
+  [command] [string]
+  
+  Available commands: lower, upper and title`
+  err = "Unknown command: %q\n"
+)
 
 func main() {
-
+  if len(os.Args) != 3 {
+    fmt.Printf(usage)
+    return
+  }
+  
+  // main
+  switch cmd, v := strings.ToLower(os.Args[1]), os.Args[2]; cmd {
+  case "lower":
+    v2 := strings.ToLower(v)
+  case "upper":
+    v2 := strings.ToUpper(v)
+  case "title":
+    v2 := strings.ToTitle(v)
+  default:
+    v2 := "Unknown command: " + v
+  }
+  
+  fmt.Printf("%s\n", v2)
 }
