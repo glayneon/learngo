babe8a0bcf73e91b95866405d0629796cf4c8677
Chase <yeraongon@gmail.com>
2022-09-06 19:18:03 +0900

    Fix it lastly.

--- 13-loops/04-nested-loops-multiplication-table/challenge/main1.go
+++ 13-loops/04-nested-loops-multiplication-table/challenge/main1.go
@@ -12,14 +12,14 @@ const max = 5
 func main() {
   // Header
   fmt.Printf("%5s", "X")
-  for i := 0 ; i <= 5; i++ {
+  for i := 0 ; i <= max; i++ {
     fmt.Printf("%5d", i)
   }
   
-  for i := 0; i <= 5; i++ {
+  for i := 0; i <= max; i++ {
     fmt.Printf("%5d", i)
     
-    for j := 0; j <= 5; j++ {
+    for j := 0; j <= max; j++ {
       fmt.Printf("%5d", i*j)
     }
     fmt.Println()
