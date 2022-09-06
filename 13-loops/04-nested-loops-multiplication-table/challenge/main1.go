0952b1398f15833001be3a84d33fa982a0e43c21
Chase <yeraongon@gmail.com>
2022-09-06 19:14:07 +0900

    Fix it finally.

--- 13-loops/04-nested-loops-multiplication-table/challenge/main1.go
+++ 13-loops/04-nested-loops-multiplication-table/challenge/main1.go
@@ -16,11 +16,12 @@ func main() {
     fmt.Printf("%5d", i)
   }
   
-  for j := 0; j <= 5; j++ {
-    fmt.Printf("%5d", j)
+  for i := 0; i <= 5; i++ {
+    fmt.Printf("%5d", i)
+    
+    for j := 0; j <= 5; j++ {
+      fmt.Printf("%5d", i*j)
+    }
     fmt.Println()
   }
-  
-  fmt.Println()
-  
 }
