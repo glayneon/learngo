6ba8e4df559c8ca487aaae4c24214bdd52959df7
Chase <yeraongon@gmail.com>
2022-09-06 18:04:50 +0900

    Add it.

---
+++ 13-loops/04-nested-loops-multiplication-table/challenge/main1.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+  "fmt"
+)
+
+
+const max = 5
+
+// main
+
+func main() {
+  // Header
+  fmt.Printf("%5s", "X")
+  for i := 0 ; i <= 5; i++ {
+    fmt.Printf("%5d", i)
+  }
+  
+  for j := 0; j <= 5; j++ {
+    fmt.Printf("%5d", j)
+  }
+  
+  fmt.Println()
+  
+}
