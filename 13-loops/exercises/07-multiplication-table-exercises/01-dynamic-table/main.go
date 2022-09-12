17710d1553d59134b047c0a834eb3b3cd1e4adf7
Chase <yeraongon@gmail.com>
2022-09-12 18:28:40 +0900

    Update the wrong grammar.

--- 13-loops/exercises/07-multiplication-table-exercises/01-dynamic-table/main.go
+++ 13-loops/exercises/07-multiplication-table-exercises/01-dynamic-table/main.go
@@ -1,44 +1,47 @@
 package main
 
 import (
-  "fmt"
-  "strconv"
-  "os"
+	"fmt"
+	"os"
+	"strconv"
 )
 
 const (
-  usage = ```
-  Give me the size of the table\n
-  ```
-  
-  err1 = "Wrong size\n"
+	usage = `
+  Give me the size of the table
+
+`
+
+	merr = "Wrong size\n"
+)
 
 func main() {
-  
-  if len(os.Args) != 2 {
-    fmt.Printf(usage)
-    return
-  }
-  
-  // check it could be converted to integer
-  if t := strconv.Atoi(os.Args[1]); t != nil || t < 0 {
-    fmt.Printf(err1)
-  } else {
-    // Print Headers
-    fmt.Printf("%5s", "X")
-    
-    // Print Headers
-    for i := 0; i <= t; i++ {
-      fmt.Printf("%5d", i)
-    }
-    
-    // Print contents of the table
-    for i := 0; i <= t; i++ {
-      fmt.Printf("%5d", i)
-      for j := 0; j <= t; j++ {
-        fmt.Printf("%5d", i*j)
-      }
-      // new line
-      fmt.Println()
-    }
+	if len(os.Args) != 2 {
+		fmt.Printf(usage)
+		return
+	}
+
+	// check it could be converted to integer
+	if t, err := strconv.Atoi(os.Args[1]); err != nil || t < 0 {
+		fmt.Printf(merr)
+	} else {
+		// Print Headers
+		fmt.Printf("%5s", "X")
+
+		// Print Headers
+		for i := 0; i <= t; i++ {
+			fmt.Printf("%5d", i)
+		}
+		fmt.Println()
+
+		// Print contents of the table
+		for i := 0; i <= t; i++ {
+			fmt.Printf("%5d", i)
+			for j := 0; j <= t; j++ {
+				fmt.Printf("%5d", i*j)
+			}
+			// new line
+			fmt.Println()
+		}
+	}
 }
