afb1b864d21fb05f321d3b7dea8bc93a32dfdf25
Chase <yeraongon@gmail.com>
2022-09-14 13:43:54 +0900

    Fix it.

--- 13-loops/exercises/03-sum-up-to-n/main.go
+++ 13-loops/exercises/03-sum-up-to-n/main.go
@@ -1,39 +1,64 @@
-// Copyright © 2018 Inanc Gumus
-// Learn Go Programming Course
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-//
-// For more tutorials  : https://learngoprogramming.com
-// In-person training  : https://www.linkedin.com/in/inancgumus/
-// Follow me on twitter: https://twitter.com/inancgumus
-
 package main
 
-// ---------------------------------------------------------
-// EXERCISE: Sum up to N
-//
-//  1. Get two numbers from the command-line: min and max
-//  2. Convert them to integers (using Atoi)
-//  3. By using a loop, sum the numbers between min and max
-//
-// RESTRICTIONS
-//  1. Be sure to handle the errors. So, if a user doesn't
-//     pass enough arguments or she passes non-numerics,
-//     then warn the user and exit from the program.
-//
-//  2. Also, check that, min < max.
-//
-// HINT
-//  For converting the numbers, you can use `strconv.Atoi`.
-//
-// EXPECTED OUTPUT
-//  Let's suppose that the user runs it like this:
-//
-//    go run main.go 1 10
-//
-//  Then it should print:
-//
-//    1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 = 55
-// ---------------------------------------------------------
+import (
+    "fmt"
+    "os"
+    "strconv"
+)
+
+const (
+    usage = `
+    
+    Usage : sumup [min] [max]
+    
+    [min] : It is an integer and it would be shorter than [max]
+    [max] : It is an integer and it would be greater than [min]
+    
+`
+    errm1 = "%q is not an integer.\n"
+    errm2 = "Missing Arguments.\n"
+    errm3 = "[min] %q must be shorther than [max].\n"
+)
 
 func main() {
+    args := os.Args[1:]
+    
+    // check command-line parameters
+    if l := len(args); l != 2 {
+        fmt.Printf(errm2)
+        fmt.Printf(usage)
+        return
+    }
+    
+    // get the min and max values and try to convert to an integer
+    min, err1 := strconv.Atoi(args[1])
+    max, err2 := strconv.Atoi(args[2])
+    
+    // check there would be an error occured.
+    if err1 != nil {
+        fmt.Printf(errm1, args[1])
+        fmt.Printf(usage)
+        return
+    } else if err2 != nil {
+        fmt.Printf(errm1, args[2])
+        fmt.Printf(usage)
+        return
+    } else if min >= max {
+        fmt.Printf(errm3, args[1])
+        fmt.Printf(usage)
+        return
+    }
+    
+    var sum int
+    for i := min; i < max; i++ {
+        fmt.Print(i)
+        
+        if i < max {
+            fmt.Print(" + ")
+        }
+        
+        sum += i
+    }
+    
+    fmt.Printf(" = %d", sum)
 }
