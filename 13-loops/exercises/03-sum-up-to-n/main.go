abbf3c238e145b76707ada07eb44564cb958f231
Chase <yeraongon@gmail.com>
2022-09-15 23:30:56 +0900

    Fix the wrong expressions.

--- 13-loops/exercises/03-sum-up-to-n/main.go
+++ 13-loops/exercises/03-sum-up-to-n/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "strconv"
+	"fmt"
+	"os"
+	"strconv"
 )
 
 const (
-    usage = `
+	usage = `
     
     Usage : sumup [min] [max]
     
@@ -15,50 +15,50 @@ const (
     [max] : It is an integer and it would be greater than [min]
     
 `
-    errm1 = "%q is not an integer.\n"
-    errm2 = "Missing Arguments.\n"
-    errm3 = "[min] %q must be shorther than [max].\n"
+	errm1 = "%q is not an integer.\n"
+	errm2 = "Missing Arguments.\n"
+	errm3 = "[min] %q must be shorther than [max].\n"
 )
 
 func main() {
-    args := os.Args[1:]
-    
-    // check command-line parameters
-    if l := len(args); l != 2 {
-        fmt.Printf(errm2)
-        fmt.Printf(usage)
-        return
-    }
-    
-    // get the min and max values and try to convert to an integer
-    min, err1 := strconv.Atoi(args[1])
-    max, err2 := strconv.Atoi(args[2])
-    
-    // check there would be an error occured.
-    if err1 != nil {
-        fmt.Printf(errm1, args[1])
-        fmt.Printf(usage)
-        return
-    } else if err2 != nil {
-        fmt.Printf(errm1, args[2])
-        fmt.Printf(usage)
-        return
-    } else if min >= max {
-        fmt.Printf(errm3, args[1])
-        fmt.Printf(usage)
-        return
-    }
-    
-    var sum int
-    for i := min; i < max; i++ {
-        fmt.Print(i)
-        
-        if i < max {
-            fmt.Print(" + ")
-        }
-        
-        sum += i
-    }
-    
-    fmt.Printf(" = %d", sum)
+	args := os.Args[1:]
+
+	// check command-line parameters
+	if l := len(args); l != 2 {
+		fmt.Printf(errm2)
+		fmt.Printf(usage)
+		return
+	}
+
+	// get the min and max values and try to convert to an integer
+	min, err1 := strconv.Atoi(args[0])
+	max, err2 := strconv.Atoi(args[1])
+
+	// check there would be an error occured.
+	if err1 != nil {
+		fmt.Printf(errm1, args[0])
+		fmt.Printf(usage)
+		return
+	} else if err2 != nil {
+		fmt.Printf(errm1, args[1])
+		fmt.Printf(usage)
+		return
+	} else if min >= max {
+		fmt.Printf(errm3, args[0])
+		fmt.Printf(usage)
+		return
+	}
+
+	var sum int
+	for i := min; i <= max; i++ {
+		fmt.Print(i)
+
+		if i < max {
+			fmt.Print(" + ")
+		}
+
+		sum += i
+	}
+
+	fmt.Printf(" = %d\n", sum)
 }
