4cb94d408745d232cc967fc0a4f032efbcf4ca5b
Chase <yeraongon@gmail.com>
2022-09-16 16:59:19 +0900

    Finish it.

--- 13-loops/exercises/06-infinite-kill/main.go
+++ 13-loops/exercises/06-infinite-kill/main.go
@@ -3,7 +3,24 @@ package main
 import (
     "fmt"
     "os"
+    "time"
+    "rand"
 )
 
 func main() {
+    const fmt = "\r%s "
+    const lc = "\/|-"
+    const dur = 250
+    const msg = "Please Wait. Processing....\n"
+    
+    for {
+        rand.Seed(time.Now().UnixNano())
+        pc := lc[rand.Intn(len(lc)+1)]
+        
+        // print
+        fmt.Printf(fmt, pc)
+        fmt.Printf(msg)
+        
+        time.Sleep(dur * time.Millisecond)
+    }
 }
