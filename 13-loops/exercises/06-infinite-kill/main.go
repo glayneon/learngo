3527b86ab4886bb23bef6664dec68be527f87534
Chase <yeraongon@gmail.com>
2022-09-16 17:13:42 +0900

    Fix it.

--- 13-loops/exercises/06-infinite-kill/main.go
+++ 13-loops/exercises/06-infinite-kill/main.go
@@ -2,13 +2,12 @@ package main
 
 import (
     "fmt"
-    "os"
     "time"
-    "rand"
+    "math/rand"
 )
 
 func main() {
-    const fmt = "\r%q "
+    const fmt = "\r%s "
     const lc = "\/|-"
     const dur = 250
     const msg = "Please Wait. Processing....\n"
