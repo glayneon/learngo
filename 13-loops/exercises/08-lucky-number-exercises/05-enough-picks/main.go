000300e7fe0622522a836bf3da324c143f794ed2
Chase <yeraongon@gmail.com>
2022-09-27 23:33:37 +0900

    Fix it again.

--- 13-loops/exercises/08-lucky-number-exercises/05-enough-picks/main.go
+++ 13-loops/exercises/08-lucky-number-exercises/05-enough-picks/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-    "fmt"
-    "time"
-    "math/rand"
-    "os"
-    "strconv"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
 )
 
 const (
-    usgage = `Welcome to the Lucky Number Game! 🍀
+	usage = `Welcome to the Lucky Number Game! 🍀
 
 The program will pick %d random numbers.
 Your mission is to guess one of those numbers.
@@ -17,44 +17,44 @@ Your mission is to guess one of those numbers.
 The greater your number is, harder it gets.
 Wanna play?
 `
-    em1 = `
+	em1 = `
     wrong number. %q
 `
-    maxTurn = 10
-    maxNum = 10
+	maxTurn = 10
+	maxNum  = 10
 )
 
 func main() {
-    args := os.Args[1:]
-    rand.Seed(time.Now().UnixNano())
-        
-    if len(args) < 1 {
-        fmt.Printf(usage, maxTurn)
-        return
-    }
-    
-    guess, err := strconv.Atoi(args[0])
-    if err != nil || guess < 0 {
-        fmt.Printf(em1, args[0])
-        return
-    }
-    
-    maxNum := 10
-    if guess > maxNum {
-        maxNum = guess
-    }
-        
-    for turn := 1; turn < maxTurn; turn++ {
-        n := rand.Intn(maxNum + 1)
-        fmt.Printf("3%d" n)
-        
-        if n == guess {
-            if turn == 1 {
-                fmt.Println("Wow, In first turn, ")
-            }
-            fmt.Println("You Win!")
-            return
-        }
-    }
-    fmt.Println("You Lost... Try again?")
+	args := os.Args[1:]
+	rand.Seed(time.Now().UnixNano())
+
+	if len(args) < 1 {
+		fmt.Printf(usage, maxTurn)
+		return
+	}
+
+	guess, err := strconv.Atoi(args[0])
+	if err != nil || guess < 0 {
+		fmt.Printf(em1, args[0])
+		return
+	}
+
+	maxNum := 10
+	if guess > maxNum {
+		maxNum = guess
+	}
+
+	for turn := 1; turn < maxTurn; turn++ {
+		n := rand.Intn(maxNum + 1)
+		fmt.Printf("%2d ", n)
+
+		if n == guess {
+			if turn == 1 {
+				fmt.Println("Wow, In first turn, ")
+			}
+			fmt.Println("You Win!")
+			return
+		}
+	}
+	fmt.Println("You Lost... Try again?")
 }
