80a0a730f36339ea894273cd2e0f3d23a441dace
Chase <yeraongon@gmail.com>
2022-09-27 10:25:17 +0900

    I made it.

--- 13-loops/exercises/08-lucky-number-exercises/01-first-turn-winner/main.go
+++ 13-loops/exercises/08-lucky-number-exercises/01-first-turn-winner/main.go
@@ -1,29 +1,49 @@
-// Copyright © 2018 Inanc Gumus
-// Learn Go Programming Course
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-//
-// For more tutorials  : https://learngoprogramming.com
-// In-person training  : https://www.linkedin.com/in/inancgumus/
-// Follow me on twitter: https://twitter.com/inancgumus
-
 package main
 
-// ---------------------------------------------------------
-// EXERCISE: First Turn Winner
-//
-//  If the player wins on the first turn, then display
-//  a special bonus message.
-//
-// RESTRICTION
-//  The first picked random number by the computer should
-//  match the player's guess.
-//
-// EXAMPLE SCENARIO
-//  1. The player guesses 6
-//  2. The computer picks a random number and it happens
-//     to be 6
-//  3. Terminate the game and print the bonus message
-// ---------------------------------------------------------
+import (
+    "fmt"
+    "time"
+    "math/rand"
+    "os"
+    "strconv"
+)
+
+const (
+    usgage = `
+    randguess [number]
+`
+    em1 = `
+    wrong number. %q
+`
+    maxTurn = 10
+)
 
 func main() {
+    args := os.Args[1:]
+    rand.Seed(time.Now().UnixNano())
+        
+    if len(args) != 1 {
+        fmt.Printf(usage)
+        return
+    }
+    
+    guess, err := strconv.Atoi(args[0])
+    if err != nil || guess < 0 {
+        fmt.Printf(em1, args[0])
+        return
+    }
+    
+    for turn := 0; turn < maxTurn; turn++ {
+        n := rand.Intn(guess + 1)
+        
+        switch {
+        case turn == 0:
+            fmt.Println("Wow, In first turn,")
+            fallthrough
+        case n == guess:
+            fmt.Println("You Win!")
+            return
+        }
+    }
+    fmt.Println("You Lost... Try again?")
 }
