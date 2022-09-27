5f38ceadd0e5095e9b8b15b51613e078f76e5262
Chase <yeraongon@gmail.com>
2022-09-27 11:01:11 +0900

    Fix it.

--- 13-loops/exercises/08-lucky-number-exercises/02-random-messages/main.go
+++ 13-loops/exercises/08-lucky-number-exercises/02-random-messages/main.go
@@ -1,38 +1,70 @@
-// Copyright © 2018 Inanc Gumus
-// Learn Go Programming Course
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-//
-// For more tutorials  : https://learngoprogramming.com
-// In-person training  : https://www.linkedin.com/in/inancgumus/
-// Follow me on twitter: https://twitter.com/inancgumus
-
 package main
 
-// ---------------------------------------------------------
-// EXERCISE: Random Messages
-//
-//  Display a few different won and lost messages "randomly".
-//
-// HINTS
-//  1. You can use a switch statement to do that.
-//  2. Set its condition to the random number generator.
-//  3. I would use a short switch.
-//
-// EXAMPLES
-//  The Player wins: then randomly print one of these:
-//
-//  go run main.go 5
-//    YOU WON
-//  go run main.go 5
-//    YOU'RE AWESOME
-//
-//  The Player loses: then randomly print one of these:
-//
-//  go run main.go 5
-//    LOSER!
-//  go run main.go 5
-//    YOU LOST. TRY AGAIN?
-// ---------------------------------------------------------
+import (
+    "fmt"
+    "time"
+    "math/rand"
+    "os"
+    "strconv"
+)
+
+const (
+    usgage = `Welcome to the Lucky Number Game! 🍀
+
+The program will pick %d random numbers.
+Your mission is to guess one of those numbers.
+
+The greater your number is, harder it gets.
+Wanna play?
+`
+    em1 = `
+    wrong number. %q
+`
+    maxTurn = 10
+)
 
 func main() {
+    args := os.Args[1:]
+    rand.Seed(time.Now().UnixNano())
+        
+    if len(args) != 1 {
+        fmt.Printf(usage, maxTurn)
+        return
+    }
+    
+    guess, err := strconv.Atoi(args[0])
+    if err != nil || guess < 0 {
+        fmt.Printf(em1, args[0])
+        return
+    }
+    
+    for turn := 1; turn < maxTurn; turn++ {
+        n := rand.Intn(guess + 1)
+        
+        switch {
+        case turn == 0:
+            fmt.Println("Wow, In first turn,")
+            fallthrough
+        case n == guess:
+            rand.Seed(time.Now().UnixNano())
+            switch rand.Intn(3); {
+            case 0:
+                fmt.Println("Winner!!")
+            case 1:
+                fmt.Println("You Win!")
+            case 2:
+                fmt.Println("Awesome!")
+            }
+            return
+        }
+    }
+    rand.Seed(time.Now().UnixNana())
+    switch rand.Intn(3); {
+    case 0:
+        fmt.Println("Loser!!")
+    case 1:
+        fmt.Println("You lost..")
+    case 2:
+        fmt.Println("Seriously??")
+    }
 }
