3d97734c05d91a1ab6b1e650409c4e1743170cd3
Chase <yeraongon@gmail.com>
2022-09-27 19:26:26 +0900

    Fix it.

--- 13-loops/exercises/08-lucky-number-exercises/05-enough-picks/main.go
+++ 13-loops/exercises/08-lucky-number-exercises/05-enough-picks/main.go
@@ -1,49 +1,60 @@
-// Copyright © 2018 Inanc Gumus
-// Learn Go Programming Course
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-//
-// For more tutorials  : https://learngoprogramming.com
-// In-person training  : https://www.linkedin.com/in/inancgumus/
-// Follow me on twitter: https://twitter.com/inancgumus
-
 package main
 
-// ---------------------------------------------------------
-// EXERCISE: Enough Picks
-//
-//  If the player's guess number is below 10;
-//  then make sure that the computer generates a random
-//  number between 0 and 10.
-//
-//  However, if the guess number is above 10; then the
-//  computer should generate the numbers
-//  between 0 and the guess number.
-//
-// WHY?
-//  This way the game will be harder.
-//
-//  Because, in the current version of the game, if
-//  the player's guess number is for example 3; then the
-//  computer picks a random number between 0 and 3.
-//
-//  So, currently a player can easily win the game.
-//
-// EXAMPLE
-//  Suppose that the player runs the game like this:
-//    go run main.go 9
-//
-//  Or like this:
-//    go run main.go 2
-//
-//    Then the computer should pick a random number
-//    between 0-10.
-//
-//  Or, if the player runs it like this:
-//    go run main.go 15
-//
-//    Then the computer should pick a random number
-//    between 0-15.
-// ---------------------------------------------------------
+import (
+    "fmt"
+    "time"
+    "math/rand"
+    "os"
+    "strconv"
+)
+
+const (
+    usgage = `Welcome to the Lucky Number Game! 🍀
+
+The program will pick %d random numbers.
+Your mission is to guess one of those numbers.
+
+The greater your number is, harder it gets.
+Wanna play?
+`
+    em1 = `
+    wrong number. %q
+`
+    maxTurn = 10
+    maxNum = 10
+)
 
 func main() {
+    args := os.Args[1:]
+    rand.Seed(time.Now().UnixNano())
+        
+    if len(args) < 1 {
+        fmt.Printf(usage, maxTurn)
+        return
+    }
+    
+    guess, err := strconv.Atoi(args[0])
+    if err != nil || guess < 0 {
+        fmt.Printf(em1, args[0])
+        return
+    }
+    
+    maxNum := 10
+    if guess > maxNum {
+        maxNum = guess
+    }
+        
+    for turn := 1; turn < maxTurn; turn++ {
+        n := rand.Intn(maxNum + 1)
+        fmt.Printf("3%d" n)
+        
+        if n == guess {
+            if turn == 1 {
+                fmt.Println("Wow, In first turn, ")
+            }
+            fmt.Println("You Win!")
+            return
+        }
+    }
+    fmt.Println("You Lost... Try again?")
 }
