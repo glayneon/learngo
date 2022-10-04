0226e0e1af62711e30853006eef241f3914a3f51
Chase <yeraongon@gmail.com>
2022-10-04 19:31:49 +0900

    Fix it.

--- 14-arrays/06-challenge-moodly/challenge/main.go
+++ 14-arrays/06-challenge-moodly/challenge/main.go
@@ -1,49 +1,41 @@
-// Copyright © 2018 Inanc Gumus
-// Learn Go Programming Course
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-//
-// For more tutorials  : https://learngoprogramming.com
-// In-person training  : https://www.linkedin.com/in/inancgumus/
-// Follow me on twitter: https://twitter.com/inancgumus
-
 package main
 
-// ---------------------------------------------------------
-// EXERCISE: Moodly
-//
-//   1. Get username from command-line
-//
-//   2. Display the usage if the username is missing
-//
-//   3. Create an array
-//     1. Add three positive mood messages
-//     2. Add three negative mood messages
-//
-//   4. Randomly select and print one of the mood messages
-//
-// EXPECTED OUTPUT
-//
-//   go run main.go
-//     [your name]
-//
-//   go run main.go Socrates
-//     Socrates feels good 👍
-//
-//   go run main.go Socrates
-//     Socrates feels bad 👎
-//
-//   go run main.go Socrates
-//     Socrates feels sad 😞
-//
-//   go run main.go Socrates
-//     Socrates feels happy 😀
-//
-//   go run main.go Socrates
-//     Socrates feels awesome 😎
-//
-//   go run main.go Socrates
-//     Socrates feels terrible 😩
-// ---------------------------------------------------------
+import (
+    "fmt"
+    "os"
+    "time"
+    "math/rand"
+)
+
+const (
+    usage = `
+    [your name]
+`
+    fmt1 = "%s feels %s\n"
+)
 
 func main() {
+    var feeling string
+    
+    react := [...]string{
+        "good",
+        "bad",
+        "sad",
+        "happy",
+        "awesome",
+        "terrible",
+    }
+    
+    rand.Seed(time.Now().UnixNano())
+    
+    if len(os.Args) != 2 {
+        fmt.Println(usage)
+        return
+    }
+    
+    feeling = react[rand.Intn(len(react))]
+    // print feeling
+    fmt.Printf(fmt1, os.Args[1], feeling)
 }
+
+    
