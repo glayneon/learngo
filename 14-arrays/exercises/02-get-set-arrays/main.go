725fb4ba6a43f400b5733000637307d730223c2a
Chase <yeraongon@gmail.com>
2022-10-14 17:02:15 +0900

    Update varaibles for arrayes.

--- 14-arrays/exercises/02-get-set-arrays/main.go
+++ 14-arrays/exercises/02-get-set-arrays/main.go
@@ -3,4 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	var (
+		names     [3]string  // The names of your three best friends
+		distances [5]int     // The distances to five different locations
+		data      [5]byte    // A data buffer with five bytes of capacity
+		ratios    [1]float64 // Currency exchange ratios only for a single currency
+		alives    [4]bool    // Up/Down status of four different web servers
+		zero      [0]byte    // A byte array that doesn't occupy memory space
+	)
+    
 }
