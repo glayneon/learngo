642c91b8dd2ea55c7e105c30455f84544d8b82ee
Chase <yeraongon@gmail.com>
2022-10-14 17:38:27 +0900

    Update the code with for statement.

--- 14-arrays/exercises/02-get-set-arrays/main.go
+++ 14-arrays/exercises/02-get-set-arrays/main.go
@@ -12,4 +12,42 @@ func main() {
 		zero      [0]byte    // A byte array that doesn't occupy memory space
 	)
 
+    // assign to names
+    names[0] = "Chase, Kim"
+    names[1] = "Triss, You"
+    names[2] = "Hans, Kim"
+    
+    // assign to distnaces
+    distances[0] = 118
+    distances[1] = 64
+    distances[2] = 1133
+    distances[3] = 443
+    distances[4] = 17818
+    
+    // assign to data
+    data[0] = "H"
+    data[1] = "E"
+    data[2] = "L"
+    data[3] = "L"
+    data[4] = "O"
+    
+    // assign to ratios
+    ratios[0] = 1.41
+    
+    // assign to alives
+    alives[0] = true
+    alives[1] = true
+    alives[2] = false
+    alives[3] = true
+    alives[4] = false
+    
+    // assign to zero
+    zero[0] = "T"
+    
+    // print names array
+    for i = 0; i < len(names)+1 ; i++ {
+        fmt.Printf("%v", i)
+    }
+    
+    
 }
