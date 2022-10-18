ab819dc0b366bf6fb94f150ef6c9c85e5ca4d263
Chase <yeraongon@gmail.com>
2022-10-18 19:29:06 +0900

    Fix it.

--- 14-arrays/exercises/04-ellipsis/main.go
+++ 14-arrays/exercises/04-ellipsis/main.go
@@ -1,26 +1,51 @@
-// Copyright © 2018 Inanc Gumus
-// Learn Go Programming Course
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-//
-// For more tutorials  : https://learngoprogramming.com
-// In-person training  : https://www.linkedin.com/in/inancgumus/
-// Follow me on twitter: https://twitter.com/inancgumus
-
 package main
 
-// ---------------------------------------------------------
-// EXERCISE: Refactor to Ellipsis
-//
-//  1. Use the 03-array-literal exercise
-//
-//  2. Refactor the length of the array literals to ellipsis
-//
-//    This means: Use the ellipsis instead of defining the array's length
-//                manually.
-//
-// EXPECTED OUTPUT
-//   The output should be the same as the 03-array-literal exercise.
-// ---------------------------------------------------------
+import "fmt"
 
 func main() {
+  names := [...]string{
+    "Chase, Kim",
+    "Triss, You",
+    "Hans, Kim",
+  }
+  distances := [...]int{118, 64, 1133, 443, 17818}
+  data := [5]byte{"H", "E", "L", "L", "O"}
+  ratios := [1]float64{1.14}
+  alives := [5]bool{
+            true,
+            true,
+            false,
+            false,
+            true,
+  }
+    
+    // print names array
+  for i = 0; i < len(names) ; i++ {
+		fmt.Printf("names[%v]: %v\n", i, names[i])
+    }
+	
+	for i = 0; i < len(distances); i++ {
+		fmt.Printf("distance[%v]: %v\n", i, distances[i])
+	}
+	
+	for i = 0; i < len(data); i++ {
+		fmt.Printf("data[%v]: %v\n", i, data[i])
+	}
+	
+	for i = 0; i < len(ratios); i++ {
+		fmt.Printf("ratios[%v]: %v\n", i, ratios[i])
+	}
+	
+	for i = 0; i < len(alives); i++ {
+		fmt.Printf("alives[%v]: %v\n", i, alives[i])
+	}
+	
+	// for range
+	for i, v := range(names) {
+		fmt.Printf("names[%v]: %v\n", i, v)
+	}
+	
+	for i, v := range(distances) {
+		fmt.Printf("distances[%v]: %v\n", i, v)
+	}
 }
