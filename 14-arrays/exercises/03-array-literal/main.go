4a1125ce99c49c1d0325755f20b69eca85bc964e
Chase <yeraongon@gmail.com>
2022-10-18 19:20:53 +0900

    Fix it.

--- 14-arrays/exercises/03-array-literal/main.go
+++ 14-arrays/exercises/03-array-literal/main.go
@@ -1,28 +1,74 @@
-// Copyright © 2018 Inanc Gumus
-// Learn Go Programming Course
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-//
-// For more tutorials  : https://learngoprogramming.com
-// In-person training  : https://www.linkedin.com/in/inancgumus/
-// Follow me on twitter: https://twitter.com/inancgumus
-
 package main
 
-// ---------------------------------------------------------
-// EXERCISE: Refactor to Array Literals
-//
-//  1. Use the 02-get-set-arrays exercise
-//
-//  2. Refactor the array assignments to array literals
-//
-//    1. You would need to change the array declarations to array literals
-//
-//    2. Then, you would need to move the right-hand side of the assignments,
-//       into the array literals.
-//
-// EXPECTED OUTPUT
-//   The output should be the same as the 02-get-set-arrays exercise.
-// ---------------------------------------------------------
+import "fmt"
 
 func main() {
+	var (
+        names     [3]string = [3]string{
+            "Chase, Kim",
+            "Triss, You",
+            "Hans, Kim",
+        }
+        distances [5]int = [5]int{118, 64, 1133, 443, 17818}
+        data      [5]byte = [5]byte{"H", "E", "L", "L", "O"}
+        ratios    [1]float64 = [1]float64{1.14}
+        alives    [4]bool = 4[bool]{
+            true,
+            true,
+            false,
+            false,
+            true,
+        }
+		zero      [0]byte    // A byte array that doesn't occupy memory space
+	)
+
+    // assign to data
+    data[0] = "H"
+    data[1] = "E"
+    data[2] = "L"
+    data[3] = "L"
+    data[4] = "O"
+    
+    // assign to ratios
+    ratios[0] = 1.41
+    
+    // assign to alives
+    alives[0] = true
+    alives[1] = true
+    alives[2] = false
+    alives[3] = true
+    alives[4] = false
+    
+    // assign to zero
+    zero[0] = "T"
+    
+    // print names array
+    for i = 0; i < len(names) ; i++ {
+		fmt.Printf("names[%v]: %v\n", i, names[i])
+    }
+	
+	for i = 0; i < len(distances); i++ {
+		fmt.Printf("distance[%v]: %v\n", i, distances[i])
+	}
+	
+	for i = 0; i < len(data); i++ {
+		fmt.Printf("data[%v]: %v\n", i, data[i])
+	}
+	
+	for i = 0; i < len(ratios); i++ {
+		fmt.Printf("ratios[%v]: %v\n", i, ratios[i])
+	}
+	
+	for i = 0; i < len(alives); i++ {
+		fmt.Printf("alives[%v]: %v\n", i, alives[i])
+	}
+	
+	// for range
+	for i, v := range(names) {
+		fmt.Printf("names[%v]: %v\n", i, v)
+	}
+	
+	for i, v := range(distances) {
+		fmt.Printf("distances[%v]: %v\n", i, v)
+	}
 }
