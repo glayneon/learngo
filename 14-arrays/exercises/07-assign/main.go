6fa984fff5207f89d0e05443d3890e345fec4d6d
Chase <yeraongon@gmail.com>
2022-10-27 16:45:16 +0900

    Fix it.

--- 14-arrays/exercises/07-assign/main.go
+++ 14-arrays/exercises/07-assign/main.go
@@ -1,45 +1,37 @@
-// Copyright © 2018 Inanc Gumus
-// Learn Go Programming Course
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-//
-// For more tutorials  : https://learngoprogramming.com
-// In-person training  : https://www.linkedin.com/in/inancgumus/
-// Follow me on twitter: https://twitter.com/inancgumus
-
 package main
 
-// ---------------------------------------------------------
-// EXERCISE: Assign the Arrays
-//
-//  1. Create an array named books
-//
-//  2. Add book titles to the array
-//
-//  3. Create two more copies of the array named: upper and lower
-//
-//  4. Change the book titles to uppercase in the upper array only
-//
-//  5. Change the book titles to lowercase in the lower array only
-//
-//  6. Print all the arrays
-//
-//  7. Observe that the arrays are not connected when they're copied.
-//
-// NOTE
-//  Check out the strings package, it has functions to convert letters to
-//  upper and lower cases.
-//
-// BONUS
-//  Invent your own arrays with different types other than string,
-//  and do some manipulations on them.
-//
-// EXPECTED OUTPUT
-//   Note: Don't worry about the book titles here, you can use any title.
-//
-//   books: ["Kafka's Revenge" "Stay Golden" "Everythingship"]
-//   upper: ["KAFKA'S REVENGE" "STAY GOLDEN" "EVERYTHINGSHIP"]
-//   lower: ["kafka's revenge" "stay golden" "everythingship"]
-// ---------------------------------------------------------
+import (
+    "fmt"
+    "strings"
+)
+
 
 func main() {
+    var (
+        upper [3]string
+        lower [3]string
+    )
+    
+    books := [3]string{
+        "Kafka's Revenge",
+        "Stay Golden",
+        "Everythingship",
+    }
+    
+    sep := "\n" + strings.Repeat("~", 30) + "\n"
+    
+    // initiate upper
+    for i, v := range books {
+        upper[i] = strings.Upper(books[i])
+    }
+    
+    // initiate lower
+    for i, v := range books {
+        lower[i] = strings.Lower(books[i])
+    }
+    
+    // print them all
+    fmt.Printf("books: %q\n", books)
+    fmt.Printf("upper: %q\n", upper)
+    fmt.Printf("lower: %q\n", lower)
 }
