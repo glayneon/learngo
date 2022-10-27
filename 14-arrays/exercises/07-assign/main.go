f5e475d3aa1c90f3251d4111c6f67f3bbf517c79
Chase <yeraongon@gmail.com>
2022-10-27 16:50:15 +0900

    Complete the exam-7

--- 14-arrays/exercises/07-assign/main.go
+++ 14-arrays/exercises/07-assign/main.go
@@ -1,37 +1,36 @@
 package main
 
 import (
-    "fmt"
-    "strings"
+	"fmt"
+	"strings"
 )
 
-
 func main() {
-    var (
-        upper [3]string
-        lower [3]string
-    )
-    
-    books := [3]string{
-        "Kafka's Revenge",
-        "Stay Golden",
-        "Everythingship",
-    }
-    
-    sep := "\n" + strings.Repeat("~", 30) + "\n"
-    
-    // initiate upper
-    for i, v := range(books) {
-        upper[i] = strings.Upper(books[i])
-    }
-    
-    // initiate lower
-    for i, v := range(books) {
-        lower[i] = strings.Lower(books[i])
-    }
-    
-    // print them all
-    fmt.Printf("books: %q\n", books)
-    fmt.Printf("upper: %q\n", upper)
-    fmt.Printf("lower: %q\n", lower)
+	var (
+		upper [3]string
+		lower [3]string
+	)
+
+	books := [3]string{
+		"Kafka's Revenge",
+		"Stay Golden",
+		"Everythingship",
+	}
+
+	// sep := "\n" + strings.Repeat("~", 30) + "\n"
+
+	// initiate upper
+	for i, _ := range books {
+		upper[i] = strings.ToUpper(books[i])
+	}
+
+	// initiate lower
+	for i, _ := range books {
+		lower[i] = strings.ToLower(books[i])
+	}
+
+	// print them all
+	fmt.Printf("books: %q\n", books)
+	fmt.Printf("upper: %q\n", upper)
+	fmt.Printf("lower: %q\n", lower)
 }
